9c6196239880c22809823f8af10aaa845e21dc20
kylejohnson-dev <160078497+kylejohnson-dev@users.noreply.github.com>
2025-08-26 12:41:54 -0500

    first commit

---
+++ src/components/blog-preview-card.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+
+export function BlogPreviewCard() {
+  return (
+    <div className="max-w-[384px] bg-white rounded-[20px] drop-shadow-[8px_8px_0px_rgba(0,0,0,1.0)] space-y-6 p-6">
+      <div className="rounded-[10px] overflow-hidden">
+        <Image
+          src="/illustration-article.svg"
+          alt="Illustration of of shapes and colors."
+          width={336}
+          height={201}
+        />
+      </div>
+      <div className="space-y-4">
+        <p className="inline-block bg-[#F4D04E] rounded text-gray-950 font-[family-name:var(--font-figtree)] font-extrabold text-[12px] leading-[150%] tracking-[0%] px-3 py-1">
+          Learning
+        </p>
+        <p className="text-gray-950 font-[family-name:var(--font-figtree)] font-medium text-[12px] leading-[150%] tracking-[0%]">
+          Published 21 Dec 2023
+        </p>
+        <h1 className="text-gray-950 font-[family-name:var(--font-figtree)] font-extrabold text-[20px] leading-[150%] tracking-[0%]">
+          HTML & CSS foundations
+        </h1>
+        <p className="text-gray-500 font-[family-name:var(--font-figtree)] font-medium text-[14px] leading-[150%] tracking-[0%]">
+          These languages are the backbone of every website, defining structure,
+          content, and presentation.
+        </p>
+      </div>
+      <div className="flex items-center space-x-3">
+        <Image
+          src="/image-avatar.webp"
+          alt="Avatar of Greg Hooper"
+          width={64}
+          height={65}
+          className="w-8 h-8 rounded-full overflow-hidden"
+        />
+        <p className="text-gray-950 font-extrabold text-[14px] leading-[150%] tracking-[0px]">
+          Greg Hooper
+        </p>
+      </div>
+    </div>
+  );
+}
